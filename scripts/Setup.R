66b0f5aac980430f1399e2a85111ae8e10ed6ee4
BaldLab <dilloncorvino@me.com>
2024-09-18 17:06:21 +0200

    desktop upload

--- scripts/Setup.R
+++ scripts/Setup.R
@@ -25,6 +25,33 @@ library("usefulfunctions")
 source("scripts/variables/Colour_scheme_variable.R", local = knitr::knit_global())
 source("scripts/variables/gene_signatures.R", local = knitr::knit_global())
 
+# ggplot2 
+# some issues with ggplot2 versions
+# Nebulosa / scCustomize wont work with v3.5.0 and therefore needs 3.4.4
+# many things can work with 3.4.4 version but
+# clusttree requres ggplot2 >= 3.5 
+
+# therefore function to change the version of ggplot2 as required so code can function
+ggplot2_switch_version <- function(desired_version) {
+  
+  library("devtools")
+  
+  # Get the current version of ggplot2
+  current_version <- packageVersion("ggplot2")
+  
+  # Convert the current version to character to compare with desired version
+  current_version <- as.character(current_version)
+  
+  # Check if the current version is different from the desired version
+  if (current_version != desired_version) {
+    print(paste("Current ggplot2 version is", current_version, ". Installing version", desired_version))
+    # Install the desired version of ggplot2
+    install_version("ggplot2", version = desired_version, repos = "http://cran.us.r-project.org")
+    print(paste("ggplot2 version", desired_version, "has been installed."))
+  } else {
+    print(paste("ggplot2 version", desired_version, "is already installed."))
+  }
+}
 
 ###############################
 # create output directories
